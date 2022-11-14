f530a91a196a9397708a61f66bad06946c660592
Marvin Arnold <marvin@geeky.rocks>
2022-11-14 16:29:07 -0600

    feat: add Open Transact styling

--- Tokens/tokentofriend/clients/src/main/webapp/src/component/App.js
+++ Tokens/tokentofriend/clients/src/main/webapp/src/component/App.js
@@ -2,7 +2,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import React from "react";
 import axios from 'axios';
 
-import '../css/App.css';
+import '../css/App.scss';
 import retrieveToken from './retrieveToken';
 import Container from 'react-bootstrap/Container'
 import { Row } from 'react-bootstrap';
@@ -51,51 +51,31 @@ class App extends React.Component {
 
     render(){
       return (
-        <div className="App">
+        <div className="App bg-primary">
           <Container>
-            <img src="https://www.corda.net/wp-content/uploads/2016/11/fg005_corda_b.png" alt="Corda" width="200" className="App-logo"/>
+            <img src="opentransact_logo.png" alt="Open Transact" width="200" className="App-logo"/>
             <Row>
-              <Col md="auto"><h1>CorDapp Token-To-Friend</h1></Col>
-              <Col md={{offset:2}}><button className="btn btn-primary disabled" >Create a Token</button> <Link to="/retrieveToken" ><button className="btn btn-primary" >Retrieve a Token</button></Link></Col>
+              <Col md="auto"><h1>Open Transact Wallet</h1></Col>
+              <Col md={{offset:2}}>
+                <Link to="/retrieveToken" ><button className="btn btn-primary" >USD Wallet</button></Link>
+                <button className="btn btn-secondary disabled" >Mint USD</button> 
+              </Col>
             </Row>
-            <p>In this example CorDapp, you will be able to issue your friend a token that carries a secret message.
-              You will receive the information of the tokenID and where the token is stored. Your friend would need these
-              information to retrieve the token and reveal the secret message.</p>
-            <p>If you are retrieving a token, please click the Retrieve Token button above to continue. ↗️ </p>
           </Container>
           <Container>
             <br />
-            <h4>Send a token to a friend: </h4>
+            <h4>Mint USD </h4>
             <form onSubmit={this.handleSubmit}>
-              <div className="form-group">
-                <label>
-                  Your Email Address
-                </label>
-                <input
-                  type="text"  className="form-control" name="seEmail" onChange={this.handleChange} />
-                <small
-                  id="emailHelp"
-                  className="form-text text-muted">
-                  We'll never share your email with anyone else.
-                </small>
-              </div>
-              <div className="form-group">
-                <label>
-                  Recipient's Email Address
-                </label>
-                <input
-                  type="text"  className="form-control" name="reEmail" onChange={this.handleChange} />
-              </div>
               <div>
-                <p>You can attache a secret message to this token and deliver to your friend. </p>
+                <p>USD amount</p>
                 <input
-                  type="text"  className="form-control" name="message" onChange={this.handleChange} />
+                  type="text"  className="form-control" name="usdAmount" onChange={this.handleChange} />
               </div>
               <br />
               <div className="row">
                 <div className="col-10" />
                 <div className="col">
-                  <button type="submit" className="btn btn-primary">Submit</button>
+                  <button type="submit" className="btn btn-primary">Execute mint</button>
                 </div>
               </div>
             </form>
