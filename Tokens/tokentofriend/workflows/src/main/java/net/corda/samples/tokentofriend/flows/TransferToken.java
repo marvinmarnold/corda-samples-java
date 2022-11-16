2a19aa70b6200bbb35a162316b7c17488e045697
Marvin Arnold <marvin@geeky.rocks>
2022-11-16 17:50:20 -0600

    transfer working

--- Tokens/tokentofriend/workflows/src/main/java/net/corda/samples/tokentofriend/flows/TransferToken.java
+++ Tokens/tokentofriend/workflows/src/main/java/net/corda/samples/tokentofriend/flows/TransferToken.java
@@ -3,35 +3,37 @@ package net.corda.samples.tokentofriend.flows;
 import co.paralleluniverse.fibers.Suspendable;
 import com.r3.corda.lib.accounts.contracts.states.AccountInfo;
 import com.r3.corda.lib.accounts.workflows.UtilitiesKt;
+import com.r3.corda.lib.accounts.workflows.flows.AccountInfoByKey;
 import com.r3.corda.lib.accounts.workflows.flows.RequestKeyForAccount;
 import com.r3.corda.lib.tokens.contracts.states.FungibleToken;
 import com.r3.corda.lib.tokens.contracts.types.IssuedTokenType;
-import com.r3.corda.lib.tokens.contracts.types.TokenType;
 import com.r3.corda.lib.tokens.contracts.utilities.AmountUtilities;
 import com.r3.corda.lib.tokens.money.MoneyUtilities;
 import com.r3.corda.lib.tokens.workflows.flows.move.MoveTokensUtilities;
-import com.r3.corda.lib.tokens.workflows.flows.rpc.IssueTokens;
-import com.r3.corda.lib.tokens.workflows.flows.rpc.MoveFungibleTokens;
 import com.r3.corda.lib.tokens.workflows.internal.flows.finality.ObserverAwareFinalityFlow;
 import com.r3.corda.lib.tokens.workflows.types.PartyAndAmount;
-import com.r3.corda.lib.tokens.workflows.utilities.FungibleTokenBuilder;
-import com.r3.corda.lib.tokens.workflows.utilities.QueryUtilities;
 import net.corda.core.contracts.Amount;
+import net.corda.core.crypto.TransactionSignature;
 import net.corda.core.flows.*;
+import net.corda.core.identity.AbstractParty;
 import net.corda.core.identity.AnonymousParty;
 import net.corda.core.identity.Party;
 import net.corda.core.node.services.Vault;
 import net.corda.core.node.services.vault.QueryCriteria;
 import net.corda.core.transactions.SignedTransaction;
 import net.corda.core.transactions.TransactionBuilder;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicReference;
 
-import static com.r3.corda.lib.tokens.workflows.utilities.QueryUtilities.sumTokenCriteria;
-import static com.r3.corda.lib.tokens.workflows.utilities.QueryUtilities.tokenAmountWithIssuerCriteria;
+import static java.util.Collections.emptyList;
 
+@InitiatingFlow
 @StartableByRPC
-public class TransferToken extends FlowLogic<String>{
+public class TransferToken extends FlowLogic<SignedTransaction>{
 
     private String senderUsername;
     private String receiverUsername;
@@ -45,7 +47,7 @@ public class TransferToken extends FlowLogic<String>{
 
     @Override
     @Suspendable
-    public String call() throws FlowException {
+    public SignedTransaction call() throws FlowException {
         Party openTransact = getOurIdentity();
         AccountInfo senderAccountInfo = UtilitiesKt.getAccountService(this).accountInfo(senderUsername).get(0).getState().getData();
         AnonymousParty senderAccount = subFlow(new RequestKeyForAccount(senderAccountInfo));
@@ -53,14 +55,13 @@ public class TransferToken extends FlowLogic<String>{
         AccountInfo receiverAccountInfo = UtilitiesKt.getAccountService(this).accountInfo(receiverUsername).get(0).getState().getData();
         AnonymousParty receiverAccount = subFlow(new RequestKeyForAccount(receiverAccountInfo));
 
-
         // move money to receiverAccountInfo account.
         final IssuedTokenType issuedUsdTokenType = new IssuedTokenType(openTransact, MoneyUtilities.getUSD());
         final Amount<IssuedTokenType> transferAmount = AmountUtilities.amount(amount, issuedUsdTokenType);
         PartyAndAmount partyAndAmount = new PartyAndAmount(receiverAccount, transferAmount);
 
         //construct the query criteria and get all available unconsumed fungible tokens which belong to senders account
-        QueryCriteria.VaultQueryCriteria criteria = new QueryCriteria.VaultQueryCriteria().withStatus(Vault.StateStatus.UNCONSUMED)
+        QueryCriteria criteria = new QueryCriteria.VaultQueryCriteria().withStatus(Vault.StateStatus.UNCONSUMED)
                 .withExternalIds(Arrays.asList(senderAccountInfo.getIdentifier().getId()));
 
         TransactionBuilder transactionBuilder = new TransactionBuilder();
@@ -72,17 +73,47 @@ public class TransferToken extends FlowLogic<String>{
         MoveTokensUtilities.addMoveFungibleTokens(transactionBuilder, getServiceHub(),
                 Arrays.asList(partyAndAmount), senderAccount, criteria);
 
-        //establish sessions with buyer and seller. to establish session get the host name from accountinfo object
-        FlowSession customerSession = initiateFlow(senderAccountInfo.getHost());
-        FlowSession dealerSession = initiateFlow(receiverAccountInfo.getHost());
+        transactionBuilder.verify(getServiceHub());
 
-        SignedTransaction partiallySignedTx = getServiceHub().signInitialTransaction(transactionBuilder, ImmutableList.of(getOurIdentity().getOwningKey()));
+        FlowSession receiverSession = initiateFlow(receiverAccountInfo.getHost());
+        SignedTransaction partiallySignedTx = getServiceHub().signInitialTransaction(transactionBuilder, Arrays.asList(getOurIdentity().getOwningKey()));
 
-        //Note: though buyer and seller are on the same node still we will have to call CollectSignaturesFlow as the signer is not a Party but an account.
-        SignedTransaction fullySignedTx = subFlow(new CollectSignaturesFlow(partiallySignedTx,
-                Arrays.asList(customerSession, dealerSession)));
+        SignedTransaction fullySignedTx = subFlow(new CollectSignaturesFlow(partiallySignedTx, Arrays.asList(receiverSession)));
+        return subFlow(new FinalityFlow(fullySignedTx, emptyList()));
+    }
 
-        //call ObserverAwareFinalityFlow for finality
-        return subFlow(new ObserverAwareFinalityFlow(fullySignedTx, Arrays.asList(customerSession, dealerSession)));
+    @InitiatedBy(TransferToken.class)
+    public static class transferTokenResponderFlow extends FlowLogic<Void> {
+
+        private final FlowSession counterParty;
+
+        public transferTokenResponderFlow(FlowSession counterParty) {
+            this.counterParty = counterParty;
+        }
+
+        @Suspendable
+        @Override
+        public Void call() throws FlowException {
+            AtomicReference<AccountInfo> receivingAccountRef = new AtomicReference<>();
+
+            SignedTransaction stx = subFlow(new SignTransactionFlow(counterParty) {
+                @Override
+                protected void checkTransaction(@NotNull SignedTransaction stx) throws FlowException {
+                    AbstractParty keyOfReceiver = stx.getCoreTransaction().outRefsOfType(FungibleToken.class).get(0)
+                            .getState().getData().getHolder();
+                    if (keyOfReceiver != null) {
+                        receivingAccountRef.set(subFlow(new AccountInfoByKey(keyOfReceiver.getOwningKey()))
+                                .getState().getData());
+                    }
+                    if (receivingAccountRef.get() == null) {
+                        throw new FlowException("Account to receive state was not found on this node");
+                    }
+                }
+            });
+
+            // record and finalize the transaction
+            SignedTransaction receivedTx = subFlow(new ReceiveFinalityFlow(counterParty, stx.getId()));
+            return null;
+        }
     }
 }
